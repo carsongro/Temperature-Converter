4e33f5fa1ecc952fb52de1191bee95346dfcc6e2
Carson <carsongro@gmail.com>
2022-05-23 16:00:43 -0600

    Initial Commit

---
+++ Conversions/ConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConversionsApp.swift
+//  Conversions
+//
+//  Created by Carson Gross on 5/23/22.
+//
+
+import SwiftUI
+
+@main
+struct ConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
