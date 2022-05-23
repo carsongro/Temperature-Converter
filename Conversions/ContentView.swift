59e250458939a96139577ee9d8215e952e800907
Carson <carsongro@gmail.com>
2022-05-23 16:55:34 -0600

    first commit

--- Conversions/ContentView.swift
+++ Conversions/ContentView.swift
@@ -8,9 +8,86 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var inputAmount = 0.0
+    @State private var inputUnit = "Celsius"
+    @State private var outputUnit = "Celsius"
+    @FocusState private var tempIsFocused: Bool
+    
+    let units = ["Celsius", "Kelvin", "Fahrenheit"]
+    
+    var toCelsius: Double {
+        if (inputUnit == "Celsius") {
+            return inputAmount
+        } else if (inputUnit == "Fahrenheit"){
+            return ((inputAmount - 32) * 5 / 9)
+        } else {
+            return (inputAmount - 273.15)
+        }
+    }
+    
+    var toOutput: Double {
+        if outputUnit == "Celsius" {
+            return toCelsius
+        } else if (outputUnit == "Kelvin") {
+            return (toCelsius + 273.15)
+        } else {
+            return ((toCelsius * 9/5) + 32)
+        }
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Temperature", value: $inputAmount, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($tempIsFocused)
+                } header: {
+                    Text("Temperature")
+                }
+                
+                Section {
+                    Picker("Input Unit", selection: $inputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                } header: {
+                    Text("From")
+                }
+                .pickerStyle(.segmented)
+                
+                Section {
+                    Picker("Input Unit", selection: $outputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                } header: {
+                    Text("To")
+                }
+                .pickerStyle(.segmented)
+                
+                Section {
+                    Text(toOutput.formatted())
+                } header: {
+                    Text("Temperature in \(outputUnit)")
+                }
+            }
+            .navigationTitle("Conversions")
+            
+            
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        tempIsFocused = false
+                    }
+                }
+            }
+        }
     }
 }
 
